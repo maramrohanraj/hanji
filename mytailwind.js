386933bf05d809b11db3adb8e0b66ca22f555bab
Maram Rohan Raj <maramrohanraj@gmail.com>
2025-02-24 00:51:23 +0530

    fix

---
+++ mytailwind.js
@@ -0,0 +1,52 @@
+/* 
+bg-red-600 => for background coloe
+p-2 => all sides padding
+pl-2,pr-2 => padding left and padding right or px-2
+pt-2,pb-2 => padding top and bottom or py-2 simple
+text-gray-600 => changing text color
+text-black => apply a black text color to an element
+shadow => it is used to apply box-shadow
+mx-auto => stand for margin-left and margin-right: auto and centers the container horizontally within its parent
+border rounded-l-md => applies the border radius to corners but only on the left side
+text-4xl => increase the font icon size
+text-sm => it applies small font-size
+text-xs => applies extra small
+rounded-full => all sides rounded border
+hidden => which mean display hidden
+hover:text-blue-600 => when hobering it will change the text-color
+bg-cover bg-no-repeat => background image will not be repeated
+font-bold => this will give bold font
+font-semibold => this will give semibold
+rounded-md => medium border radius(6px)
+flex flex-1 => grow the first element
+grid grid-cols-3 => here we will give 3 columns of same size
+gap-4 => it will give gap b/w the cols
+hover:scale-110 when hovering increase the image size 
+flex-row,flex-col,flex-row-reverse,flex-col-reverse for the flex direction
+bg-opacity-40 => semi transparent more opacity less visibility
+in parent we should give group class and in child give group:hover to make the child visible when we hover on the parent
+in parent we should give class relative group and then in the child we will give absolute inset-0 bg-black
+whitespace-nowrap => it prevents the text from wrapping
+max-w-xl => set the max width
+overflow-hidden
+hover:underline
+no-underline => text decoration none;
+underline => text decoration underline
+focus:outline-none
+border-collapse
+float-left,float-right
+object-cover => to cover total container 
+flex-nowrap,flex-wrap,flex-wrap-reverse
+flex-1 => to grow or shrink
+grow => to grow one item to any avaialbel space
+order-1,order-2,order-3,order-first,order-last => to order the flex elements
+col-start-2,col-end-4,col-span-2
+grid-cols-[1fr_2fr] => 2 columns of size
+grid justify-items-center,start,end
+tracking-widest => for letter spacing
+list-disc => list style type disc
+uppercase,lowercase => for text transform
+blur-xs
+border-collapse,border-separate
+border-spacing-2
+*/
---
+++ notes.js
@@ -0,0 +1,6 @@
+/* 
+* FOR INSTALLATION : npm install -D tailwindcss@3.4.17
+* FOR INITIALIZATION :  npx tailwindcss init 
+* in input.css write the @tailwind base,@tailwind components , @tailwind utilities and change the language mode
+* FOR running  : npx tailwindcss -i "./input.css" -o "./style.css" --watch
+*/
